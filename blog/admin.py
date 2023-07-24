26785c8adc60cde9815bc3c35ffc3c23ec94e908
parksanghyeon <jhj80271@gmail.com>
2023-07-24 18:26:55 +0900

    FBV로 블로그 포스트 목록 페이지 만들기

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from .models import Post
 
 admin.site.register(Post)
 # Register your models here.
-=======
 
 # Register your models here.
->>>>>>> 9db7e0f14ca431b9693eaceff4b6023b24bb891c
